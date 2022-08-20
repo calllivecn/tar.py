f5129597528a9576204a4c620a0f6fe2d6d3815f
calllivecn <calllivecn@outlook.com>
2022-08-20 12:34:04 +0800

    创建可以了

--- src/libargparse.py
+++ src/libargparse.py
@@ -8,6 +8,7 @@ import re
 import sys
 import glob
 import argparse
+from pathlib import Path
 
 import util
 
@@ -61,6 +62,13 @@ def split_size(unit_size):
     else:
         raise argparse.ArgumentTypeError(f"不支的切割单位, 必须是: {unit_chars}")
 
+def target_exists(filename):
+    p = Path(filename)
+    if p.exists():
+        return p
+    else:
+        raise argparse.ArgumentTypeError(f"{p} 不存在")
+
 
 def exclude(glob_list):
     # glob
@@ -83,7 +91,7 @@ POXIS tar 工具
 '''.format(sys.argv[0])
 
 
-def parse_args(Description):
+def parse_args():
 
     parse = Argument(
         usage="%(prog)s [option] [file ... or directory ...]",
@@ -94,27 +102,30 @@ def parse_args(Description):
         )
     
     # 位置参数
-    parse.add_argument('target', nargs='*', help='文件s | 目录s')
+    parse.add_argument('target', nargs='*', type=target_exists, help='文件s | 目录s')
 
     parse.add_argument("-h", "--help", action="store_true", help="输出帮助信息")
 
-    parse.add_argument("-f", help="tar 文件")
-    parse.add_argument("-C", help="解压到指定目录(default: 当前目录)")
+    parse.add_argument("-f", type=Path, help="archive 文件, 没有这参数时，默认使用标准输入输出。")
+    parse.add_argument("-C", type=target_exists, help="更改目录(default: .)")
+
     # 从标准输入读取
-    parse.add_argument("--stdin", action="store_true", help="从标准输入读取")
-    parse.add_argument("--stdout", action="store_true", help="输出到标准输出")
+    # parse.add_argument("--stdin", action="store_true", help="从标准输入读取")
+    # parse.add_argument("--stdout", action="store_true", help="输出到标准输出")
+    parse.add_argument("-O", action="store_true", help="解压文件至标准输出")
 
     group1 = parse.add_mutually_exclusive_group()
     group1.add_argument("-c", action="store_true", help="创建tar文件")
     group1.add_argument("-x", action="store_true", help="解压tar文件")
-    # group1.add_argument('-x', '--extract', action='store_true', help='extract files from an archive')
     group1.add_argument("-t", "--list", action="store_true", help="输出tar文件内容")
+
+    parse.add_argument("--safe-extract", dest="safe_extract", action="store_true", help="处理tar里不安全的路径后在解压")
     parse.add_argument("-v", "--verbose", action="count", help="输出详情")
 
-    parse.add_argument("--exclude", metavar="PATTERN", nargs="+", type=exclude, help="排除这类文件,使用 glob: PATTERN")
-    parse.add_argument("--exclude-regex", metavar="PATTERN", nargs="+", type=exclude_regex, help="排除这类文件, 使用正则 PATTERN")
+    parse.add_argument("--exclude", dest="exclude", metavar="PATTERN", nargs="+", type=exclude, help="排除这类文件,使用 glob: PATTERN")
+    parse.add_argument("--exclude-regex", dest="exclude_regex", metavar="PATTERN", nargs="+", type=exclude_regex, help="排除这类文件, 使用正则 PATTERN")
 
-    # 这个工具只支持解压这这些，创建时只使用zstd
+    # 这个工具只支持解压这些，创建时只使用zstd
     # group2 = parse.add_mutually_exclusive_group()
     # group2.add_argument('-z', '--gzip', action='store_true', help='filter the archive through gzip')
     # group2.add_argument('-j', '--bzip2', action='store_true', help='filter the archive through bzip2')
@@ -122,24 +133,26 @@ def parse_args(Description):
 
     parse_compress = parse.add_argument_group("压缩选项", description="只使用zstd压缩方案, 但可以解压 gz, bz2, xz。")
     parse_compress.add_argument("-z", action="store_true", help="使用zstd压缩(default: level=3)")
-    parse_compress.add_argument("-l", metavar="level", type=compress_level, default=3, help="指定压缩level: 1 ~ 22")
-    parse_compress.add_argument("-T", metavar="threads", type=int, default=util.cpu_physical(), help="默认使用全部物理CPU核心")
+    parse_compress.add_argument("-l", dest="level", metavar="level", type=compress_level, default=3, help="指定压缩level: 1 ~ 22")
+    parse_compress.add_argument("-T", dest="threads", metavar="threads", type=int, default=util.cpu_physical(), help="默认使用全部CPU物理核心")
 
 
-    parse_encrypt = parse.add_argument_group("加密", description="目前只使用aes-256-cfb")
+    parse_encrypt = parse.add_argument_group("加密", description="使用aes-256-cfb加密算法")
     parse_encrypt.add_argument("-e", action="store_true", help="加密")
-    parse_encrypt.add_argument("-k", metavar="PASSWORK", action="store", help="指定密码 (default：启动后交互式输入)")
+    parse_encrypt.add_argument("-k", type=str, metavar="PASSWORK", action="store", help="指定密码 (default：启动后交互式输入)")
     # parse_encrypt.add_argument("-d", action="store_true", help="解密")
     parse_encrypt.add_argument("--prompt", help="密码提示信息")
 
     parse_hash = parse.add_argument_group("计算输出文件的sha值")
-    parse_hash.add_argument("--sha-file", metavar="FILENAME", action="store", help="哈希值输出到文件(default: stderr)")
+    # parse_hash.add_argument(dest="shafuncs", default=set(), help=argparse.SUPPRESS)
+    parse_hash.add_argument("--sha-file", dest="sha_file", metavar="FILENAME", action="store", help="哈希值输出到文件(default: stderr)")
     parse_hash.add_argument("--md5", action="store_true", help="下载同时计算 md5")
     parse_hash.add_argument("--sha1", action="store_true", help="下载同时计算 sha1")
     parse_hash.add_argument("--sha224", action="store_true", help="下载同时计算 sha224")
-    parse_hash.add_argument("--sha256", action="store_true", help="下载同时计算 sha256")
+    parse_hash.add_argument("--sha256", action="store_true", help="下载同时计算 default: sha256")
     parse_hash.add_argument("--sha384", action="store_true", help="下载同时计算 sha384")
     parse_hash.add_argument("--sha512", action="store_true", help="下载同时计算 sha512")
+    parse_hash.add_argument("--blake2b", action="store_true", help="下载同时计算 blake2b")
     parse_hash.add_argument("--sha-all", action="store_true", help="计算下列所有哈希值")
 
     parse_split = parse.add_argument_group("切割输出文件")
--- src/tar.py
+++ src/tar.py
@@ -7,12 +7,96 @@
 
 import os
 import sys
+import getpass
+from threading import Thread
+
 
 import util
-
 from libargparse import (
     parse_args,
 )
+
+def create(args, shafuncs):
+    # 收集关闭read的close2()。
+    pipes = []
+
+    # 在主线程执行完的时候，分支线程可能 没执行完。需要收集分支线程，在 close2() 之前 join()
+    fork_threads = []
+
+    p = util.Pipe()
+    th1 = Thread(target=util.tar2pipe, args=(args.target, p), name="tar 2 pipe", daemon=True)
+    th1.start()
+    pipes.append(p)
+
+    if args.z:
+        p2 = util.Pipe()
+        th2 = Thread(target=util.compress, args=(p, p2, args.level, args.threads), name="zstd", daemon=True)
+        th2.start()
+        p = p2
+        pipes.append(p)
+
+    if args.e:
+        p3 = util.Pipe()
+
+        if args.k:
+            password = args.k
+        else:
+            password = getpass.getpass("Password:")
+            password2 = getpass.getpass("Password(again):")
+            if password != password2:
+                print("password mismatches.")
+                sys.exit(2)
+
+        th3 = Thread(target=util.encrypt, args=(p, p3, password, args.prompt), name="encrypt", daemon=True)
+        th3.start()
+        p = p3
+        pipes.append(p)
+
+    """
+    if args.split:
+        p2 = util.Pipe()
+        th2 = Thread(target=util.split, args=(p1, p2, args.level, args.T), daemon=True)
+        th2.start()
+        p2.close()
+    """
+
+    if len(shafuncs) > 0:
+        fork = util.Pipefork()
+        p4 = fork.fork()
+        sha = fork.fork()
+
+        # 从里把管道流分成两条
+        th_fork = Thread(target=util.to_pipe, args=(p, fork), name="to pipe", daemon=True)
+        th_fork.start()
+        p = p4
+
+        th4 = Thread(target=util.shasum, args=(shafuncs, sha, args.sha_file), name="shasum", daemon=True)
+        th4.start()
+        fork_threads.append(th4)
+        pipes.append(fork)
+
+    
+    # 最后写写入到文件, 还需要处理输出到标准输出的
+    if args.f:
+        f = open(args.f, "wb")
+        th_end = Thread(target=util.to_file, args=(p, f), name="to file", daemon=True)
+    else:
+        f = sys.stdout.buffer
+        th_end = Thread(target=util.to_file, args=(p, f), name="to stdout", daemon=True)
+
+    th_end.start()
+    [ th.join() for th in fork_threads]
+    [ p.close2() for p in pipes]
+
+    th_end.join()
+    f.close()
+
+def extract(args):
+    
+    if args.f:
+        if args.f.suffx() == .gz:
+        p = util.Pipe()
+        th1 = Thread(target=util.extract, args=())
 
 
 def main():
@@ -25,6 +109,45 @@ def main():
     if args.parse:
         print(args)
         sys.exit(0)
+
+    # hash 算计
+    shafuncs = set()
+    # shafuncs = args.shafuncs # 初步尝试不行
+    if args.md5:
+        shafuncs |= {"md5"}
+
+    if args.sha1:
+        shafuncs |= {"sha1"}
+
+    if args.sha224:
+        shafuncs |= {"sha224"}
+
+    if args.sha256:
+        shafuncs |= {"sha256"}
+
+    if args.sha384:
+        shafuncs |= {"sha384"}
+
+    if args.sha512:
+        shafuncs |= {"sha512"}
+    
+    if args.blake2b:
+        shafuncs |= {"blake2b"}
+    
+    if args.sha_all:
+        shafuncs |= set(("md5", "sha1", "sha224", "sha256", "sha384", "sha512", "blake2b"))
+    
+    if args.C:
+        os.chdir(args.C)
+
+    # 创建archive
+    if args.c:
+        if len(args.target) == 0:
+            print(f"{sys.argv[0]}: 谨慎地拒绝创建空归档文件", file=sys.stderr)
+            return 
+        create(args, shafuncs)
+    else:
+        extract(args, shafuncs)
 
 
 if __name__ == "__main__":
