553adb3ee7b63fbe2810893c731444014a229645
calllivecn <calllivecn@outlook.com>
2024-01-30 04:06:31 +0800

    添加测试单元 的开始。

---
+++ src/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# date 2024-01-30 03:47:12
+# author calllivecn <c-all@qq.com>
+
+
+import unittest
+
+import version
+
+
+class MainTestCase(unittest.TestCase):
+
+    def test_version(self):
+        self.assertTrue(hasattr(version, "VERSION"), True)
+    
+    def test_pass(self):
+        pass
